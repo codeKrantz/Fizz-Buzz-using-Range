eb4c2c601117a2e0f1f109604906460fb686b9d3
Conrad Krantz <conradkrantz@gmail.com>
2023-08-02 10:15:06 -0500

    Moved my python file into the repository

---
+++ RangeFizzBuzz.py
@@ -0,0 +1,25 @@
+# range of numbers 1-100
+num_range = range(1, 101)
+# converting range into a list
+num_list = list(num_range)
+#CREATING THE FIZZ BUZZ FEATURE
+for n in num_list:
+    #for fizz buzz
+    if n % 3 and n % 7 == 0:
+
+        print("Fizz Buzz")
+
+    # for fizz
+    elif n % 3 == 0:
+
+        print("Fizz")
+
+    # for buzz
+    elif n % 7 ==0:
+
+        print("Buzz")    
+
+# for the rest of the numbers
+    else:
+        
+        print(n)
